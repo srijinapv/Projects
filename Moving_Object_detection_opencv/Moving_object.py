e6130260f2c918a0326d08ed1f412f98fcf6b0e1
srijina <srijinapv@yahoo.com>
2021-10-17 23:59:23 +0530

    adding commands

--- Moving_Object_detection_opencv/Moving_object.py
+++ Moving_Object_detection_opencv/Moving_object.py
@@ -1,20 +1,34 @@
 import numpy as np
 import cv2
-import argparse
 
-def background(path):
-    video_input =cv2.VideoCapture(path)
-    frame =video_input.get(cv2.CAP_PROP_FRAME_COUNT)*np.random.uniform(size = 50)
-    number_frame = []
-    for i in frame:
-        video_input.set(cv2.CAP_PROP_POS_FRAMES, i)
-        ret, captured_frame = video_input.read()
-        number_frame.append(captured_frame)
-    # calculate the median
-    median_frame = np.median(number_frame, axis=0).astype(np.uint8)
+%matplotlib inline
+from matplotlib import pyplot as plt
 
-    return median_frame
+np.random.seed(42)
+
+video_stream = cv2.VideoCapture('/input/video_3.mp4')
+
+# Randomly select 30 frames
+frameIds = video_stream.get(cv2.CAP_PROP_FRAME_COUNT) * np.random.uniform(size=30)
+
+# Store selected frames in an array
+frames = []
+for fid in frameIds:
+    video_stream.set(cv2.CAP_PROP_POS_FRAMES, fid)
+    ret, frame = video_stream.read()
+    frames.append(frame)
+
+video_stream.release()
+
+# Calculate the median along the time axis
+medianFrame = np.median(frames, axis=0).astype(dtype=np.uint8)
+medianFrame_RGB = cv2.cvtColor(medianFrame, cv2.COLOR_BGR2RGB)
+plt.imshow(medianFrame_RGB)
 
 # Calculate the average along the time axis
-avgFrame = np.average(number_frame, axis=0).astype(dtype=np.uint8)
-plt.imshow((avgFrame))
+avgFrame = np.average(frames, axis=0).astype(dtype=np.uint8)
+avgFrame_RGB = cv2.cvtColor(avgFrame, cv2.COLOR_BGR2RGB)
+plt.imshow((avgFrame_RGB))
+
+
+

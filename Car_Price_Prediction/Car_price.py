7d2127e6da4ba149da1ae6c0893209498339d747
srijina <srijinapv@yahoo.com>
2021-11-17 23:08:08 +0530

    EDA of car price prediction

--- Car_Price_Prediction/Car_price.py
+++ Car_Price_Prediction/Car_price.py
@@ -12,3 +12,9 @@ df.describe()
 df.shape
 #checking for null values
 df.isnull().sum()
+
+cat_val = df[['CarName', 'fueltype', 'aspiration','doornumber','carbody','drivewheel','enginelocation','enginetype','cylindernumber','fuelsystem']].copy()
+
+for i in cat_val:
+    count_plot = sns.countplot(x = cat_val[i], data = cat_val)
+    plt.show()

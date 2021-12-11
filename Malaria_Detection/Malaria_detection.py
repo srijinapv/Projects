f7156c9d35215a01ee9f2c8a902346a0d16fd0c7
srijina <srijinapv@yahoo.com>
2021-12-11 23:59:18 +0530

    Detection of Malaria

---
+++ Malaria_Detection/Malaria_detection.py
@@ -0,0 +1,12 @@
+#import the libraries as shown below
+
+from tensorflow.keras.layers import Input, Lambda, Dense, Flatten,Conv2D
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications.vgg19 import VGG19
+from tensorflow.keras.applications.resnet50 import preprocess_input
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator,load_img
+from tensorflow.keras.models import Sequential
+import numpy as np
+from glob import glob
+import matplotlib.pyplot as plt

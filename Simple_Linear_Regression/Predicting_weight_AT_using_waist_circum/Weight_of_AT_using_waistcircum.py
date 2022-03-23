e8922eea35adf37079c277dc3b75213d641f48bc
srijina <srijinapv@yahoo.com>
2022-03-24 00:01:35 +0530

    Predicted Adipose Tissue using Waist circumference

--- Simple_Linear_Regression/Predicting_weight_AT_using_waist_circum/Weight_of_AT_using_waistcircum.py
+++ Simple_Linear_Regression/Predicting_weight_AT_using_waist_circum/Weight_of_AT_using_waistcircum.py
@@ -60,3 +60,111 @@ model2 = smf.ols('AT ~ np.log(Waist)', data = data).fit()
 model2.summary()
 
 pred2 = model2.predict(pd.DataFrame(data['Waist']))
+
+# Regression Line
+plt.scatter(np.log(data.Waist), data.AT)
+plt.plot(np.log(data.Waist), pred2, "r")
+plt.legend(['Predicted line', 'Observed data'])
+plt.show()
+
+# Error calculation
+res2 = data.AT - pred2
+res_sqr2 = res2 * res2
+mse2 = np.mean(res_sqr2)
+rmse2 = np.sqrt(mse2)
+rmse2
+
+#### Exponential transformation
+# x = waist; y = log(at)
+
+plt.scatter(x = data['Waist'], y = np.log(data['AT']), color = 'green')
+np.corrcoef(data.Waist, np.log(data.AT)) #correlation
+
+model3 = smf.ols('np.log(AT) ~ Waist', data = data).fit()
+model3.summary()
+
+pred3 = model3.predict(pd.DataFrame(data['Waist']))
+pred3_at = np.exp(pred3)
+pred3_at
+
+# Regression Line
+plt.scatter(data.Waist, np.log(data.AT))
+plt.plot(data.Waist, pred3, "r")
+plt.legend(['Predicted line', 'Observed data'])
+plt.show()
+
+# Error calculation
+res3 = data.AT - pred3_at
+res_sqr3 = res3 * res3
+mse3 = np.mean(res_sqr3)
+rmse3 = np.sqrt(mse3)
+rmse3
+
+
+#### Polynomial transformation
+# x = waist; x^2 = waist*waist; y = log(at)
+
+model4 = smf.ols('np.log(AT) ~ Waist + I(Waist*Waist)', data = data).fit()
+model4.summary()
+
+pred4 = model4.predict(pd.DataFrame(data))
+pred4_at = np.exp(pred4)
+pred4_at
+
+# Regression line
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 2)
+X = data.iloc[:, 0:1].values
+X_poly = poly_reg.fit_transform(X)
+# y = wcat.iloc[:, 1].values
+
+
+plt.scatter(data.Waist, np.log(data.AT))
+plt.plot(X, pred4, color = 'blue')
+plt.legend(['Predicted line', 'Observed data'])
+plt.show()
+
+
+# Error calculation
+res4 = data.AT - pred4_at
+res_sqr4 = res4 * res4
+mse4 = np.mean(res_sqr4)
+rmse4 = np.sqrt(mse4)
+rmse4
+
+
+#choose the best model using RMSE
+data1 = {"MODEL":pd.Series(["SLR","Log Model", "Exp Model", "Poly Model"]), "RMSE":pd.Series([rmse1,rmse2,rmse3,rmse4])}
+table_rmse = pd.DataFrame(data1)
+table_rmse
+
+#Best model
+from sklearn.model_selection import train_test_split
+train ,test = train_test_split(data, test_size = 0.2)
+finalmodel = smf.ols('np.log(AT)~ Waist + I( Waist*Waist)', data = train).fit()
+finalmodel.summary()
+
+#Predict on test data
+test_pred = finalmodel.predict(pd.DataFrame(test))
+pred_test_AT = np.exp(test_pred)
+pred_test_AT
+
+#Model Evaluation on Test data
+test_res = test.AT - pred_test_AT
+test_sqrs = test_res * test_res
+test_mse = np.mean(test_sqrs)
+test_rmse = np.sqrt(test_mse)
+test_rmse
+
+# Prediction on train data
+train_pred = finalmodel.predict(pd.DataFrame(train))
+pred_train_AT = np.exp(train_pred)
+pred_train_AT
+
+# Model Evaluation on train data
+train_res = train.AT - pred_train_AT
+train_sqrs = train_res * train_res
+train_mse = np.mean(train_sqrs)
+train_rmse = np.sqrt(train_mse)
+train_rmse
+
